609f43828ddc27302111e86b6e9a3ae72366abf4
Emilia Serafin <emiserafin14@gmail.com>
2024-01-16 09:46:02 -0300

    Corregidos errores en el script, ahora funciona correctamente.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const buttonContainer = document.querySelector(".button-container");
 const display = document.querySelector(".display");
 const operadores = ['+', '-', 'x', '/'];
-let terminos = ['0', '', ''];
+let termino1 = 0;
+let operador = '';
+let termino2 = '';
 
 const suma = (a, b) => a + b;
 const resta = (a, b) => a - b;
@@ -9,16 +11,18 @@ const multiplicacion = (a, b) => a * b;
 const division = (a, b) => (b !== 0) ? a / b : 'Error';
 
 const borrar = () => {
-  display.innerText = "";
-  terminos = ['0', '', ''];
+  display.innerText = 0;
+  termino1 = '';
+  operador = '';
+  termino2 = '';
 };
 
 const borrarUltimo = () => {
   display.innerText = display.innerText.slice(0, -1);
 };
 
-const calcular = (operacion, a, b) => {
-  switch (operacion) {
+const calcular = (operador, a, b) => {
+  switch (operador) {
     case '+':
       return suma(a, b);
     case '-':
@@ -32,39 +36,60 @@ const calcular = (operacion, a, b) => {
   }
 };
 
+const checkNull = (termino)=>{
+  if (termino === ''){
+    return true; 
+  } else {
+    return false;
+  }
+}
+
+// Si la pantalla esta vacia muestra 0.
+display.innerText = display.innerText === '' ? 0 : display.innerText;
+
 buttonContainer.addEventListener("click", (event) => {
+  // Controla que solo traiga la info de los botones.
   if (event.target.tagName === 'BUTTON') {
     const buttonText = event.target.innerText;
-
+    // Si presiona un numero lo escribe en la pantalla.
     if (!isNaN(parseInt(buttonText))) {
+      // Si la pantalla muestra 0, lo reemplaza por el numero presionado.
+      display.innerText = display.innerText === '0' ? buttonText :
       display.innerText += buttonText;
     } else if (buttonText === 'C') {
       borrar();
     } else if (buttonText === '←') {
       borrarUltimo();
-    } else if (operadores.includes(buttonText)) {
-      if (terminos[0].length > 0 && terminos[1].length > 0 && display.innerText.length > 0) {
-        terminos[2] = display.innerText;
-        const resultadoParcial = calcular(terminos[1], parseInt(terminos[0]), parseInt(terminos[2]));
-        display.innerText = resultadoParcial;
-        terminos = [resultadoParcial, buttonText, ''];
-      } else if (terminos[0].length > 0 && display.innerText.length > 0) {
-        terminos[0] = display.innerText;
-        terminos[1] = buttonText;
-        display.innerText = '';
+    } 
+    // Si presiona un operador y el operador no esta cargado, hace una operacion simple.
+    else if (operadores.includes(buttonText)) {
+      if (checkNull(operador)) {
+        operador = buttonText;
+        termino1 = parseInt(display.innerText);
       }
-    } else if (buttonText === '=') {
-      if (terminos[0].length > 0 && terminos[1].length > 0 && display.innerText.length > 0) {
-        terminos[2] = display.innerText;
-        display.innerText = '';
-        const resultadoFinal = calcular(terminos[1], parseInt(terminos[0]), parseInt(terminos[2]));
-        if (isNaN(resultadoFinal)) {
-          display.innerText = 'Error';
-        } else {
-          display.innerText = resultadoFinal;
-          terminos = [resultadoFinal, '', ''];
-        }
+      else {
+        const result = calcular(operador, termino1, parseInt(termino2))
+        display.innerText = result ;
+        termino1 = result ;
+        operador = buttonText;
+        termino2 = '';
       }
     }
+
+    // Si presiona un numero, el operador y el termino1 estan cargados, carga el termino2.
+    if (!checkNull(operador) && !checkNull(termino1) && !isNaN(parseInt(buttonText))) {
+      display.innerText = display.innerText === '0' ? buttonText :
+      termino2 += buttonText;
+    }
+
+    // Si presiona el igual, calcula el resultado.
+    if (buttonText === '=') {
+      const result = calcular(operador, termino1, parseInt(termino2))
+      display.innerText = result ;
+      termino1 = result ;
+      operador = '';
+      termino2 = '';
+    }
+
   }
 });
